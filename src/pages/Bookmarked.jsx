81b0c6409def9b09aab6d8fae38ab76342605b9d
Artur Wiśniewski <artixbtw@gmail.com>
2023-09-18 20:11:31 +0200

    add Styles to Error element

--- src/pages/Bookmarked.jsx
+++ src/pages/Bookmarked.jsx
@@ -10,15 +10,26 @@ export function Bookmarked() {
   const onlyBookmarkedTvSeries = movies.filter(
     (item) => item.isBookmarked === true && item.category === "TV Series"
   );
+
+  const BookmarkedMoviesLengthZero = onlyBookmarkedMovies.length > 0;
+  const BookmarkedTvSeriesLengthZero = onlyBookmarkedTvSeries.length > 0;
   return (
     <>
-      <h2 className="listContainer__h2">Bookmarked Movies</h2>
+      {BookmarkedMoviesLengthZero ? (
+        <h2 className="listContainer__h2">Bookmarked Movies</h2>
+      ) : (
+        <h2 className="listContainer__h2">Add some favourite movies</h2>
+      )}
       <div className="listContainer">
         {onlyBookmarkedMovies.map((movieItem, index) => (
           <AllMovieItem item={movieItem} key={index} />
         ))}
       </div>
-      <h2 className="listContainer__h2 mt">Bookmarked TV Series</h2>
+      {BookmarkedTvSeriesLengthZero ? (
+        <h2 className="listContainer__h2">Bookmarked TV Series</h2>
+      ) : (
+        <h2 className="listContainer__h2">Add some favourite TV Series</h2>
+      )}
       <div className="listContainer">
         {onlyBookmarkedTvSeries.map((tvSeriesItem, index) => (
           <AllMovieItem item={tvSeriesItem} key={index} />
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,3 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export function Error() {
-  return <h1>Error</h1>;
+  const navigate = useNavigate();
+  return (
+    <div className="error">
+      <h3 className="error__h3">Ups... Something went wrong!</h3>
+      <button onClick={() => navigate(-1)} className="error__btn">
+        {" "}
+        &larr; Go back!{" "}
+      </button>
+    </div>
+  );
 }
