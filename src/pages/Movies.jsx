9bc16411cb2e4111956d1f770ee295a8f47b0626
Artur Wiśniewski <artixbtw@gmail.com>
2023-09-18 19:16:30 +0200

    add logic to Movie and TvSeries

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,14 +1,20 @@
-import data from "../data.json";
 import OnlyMoviesItem from "../components/OnlyMoviesItem";
+import { useOutletContext } from "react-router-dom";
 export default function Movies() {
-  const onlyMovies = data.filter((item) => item.category === "Movie");
+  const { movies, searchTitle } = useOutletContext();
+
+  const onlyMovies = movies.filter((item) => item.category === "Movie");
+
+  const filteredMovies = onlyMovies.filter((movie) =>
+    movie.title.toLocaleLowerCase().includes(searchTitle.toLocaleLowerCase())
+  );
 
   return (
     <>
       <h2 className="listContainer__h2">Recommended for you</h2>
       <div className="listContainer">
-        {onlyMovies.map((item, index) => (
-          <OnlyMoviesItem item={item} key={index} />
+        {filteredMovies.map((item) => (
+          <OnlyMoviesItem item={item} key={item.id} />
         ))}
       </div>
     </>
--- src/pages/TvSeries.jsx
+++ src/pages/TvSeries.jsx
@@ -1,14 +1,19 @@
-import data from "../data.json";
+import { useOutletContext } from "react-router-dom";
 import OnlyTvSeriesItem from "../components/OnlyTvSeriesItem";
 
 export default function Movies() {
-  const onlyTvSeries = data.filter((item) => item.category === "TV Series");
+  const { movies, searchTitle } = useOutletContext();
+  const onlyTvSeries = movies.filter((item) => item.category === "TV Series");
+
+  const filteredMovies = onlyTvSeries.filter((movie) =>
+    movie.title.toLocaleLowerCase().includes(searchTitle.toLocaleLowerCase())
+  );
 
   return (
     <>
       <h2 className="listContainer__h2">Recommended for you</h2>
       <div className="listContainer">
-        {onlyTvSeries.map((item, index) => (
+        {filteredMovies.map((item, index) => (
           <OnlyTvSeriesItem item={item} key={index} />
         ))}
       </div>
